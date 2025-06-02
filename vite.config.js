54c7ccaef18bfe62b22c15b48c98b5509ed0c0fd
Sergey <gpgmeilry@gmail.com>
2025-06-02 18:09:56 +0300

    fixed the base value in the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "/zpi-zp41_appImgSerach-ChepurnovSerhii-MEU-2025/",
   define: {
     global: 'window',
-    base: "zpi-zp41_appImgSerach-ChepurnovSerhii-MEU-2025",
   }
 });
